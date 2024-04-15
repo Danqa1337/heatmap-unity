8c04e11990c356e67064e0ce49a02e7a29f9e316
Danqa <Ldk99@yandex.ru>
2024-04-15 16:25:44 +0300

    Added touch recorder and moved assets

---
+++ Assets/HeatMapUnity/Scripts/TouchRecorder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TouchRecorder : MonoBehaviour
+{
+    [SerializeField]
+    private string dataPath;
+    [SerializeField]
+    private string eventName;
+    [SerializeField]
+    private bool createFileIfNonFound;
+    private IEventWriter eventWriter;
+    private bool _recording;
+
+    void Awake()
+    {
+        eventWriter = new JSONEventWriter(Application.dataPath + dataPath, createFileIfNonFound);
+    }
+    private void Update()
+    {
+        if (_recording)
+        {
+            if (Input.GetKey(KeyCode.Mouse0) || Input.touchCount > 0)
+            {
+                var input = Input.mousePosition;
+                if(Input.touches.Length > 0)
+                {
+                    input = (Vector3)Input.touches[0].position;
+                }
+
+                BaseEvent baseEvent = new BaseEvent(eventName, new Vector3(input.x, 0, input.y) / 100f);
+                eventWriter.SaveEvent(baseEvent);
+            } 
+        }
+    }
+    public void StopRecording()
+    {
+        _recording = false;
+    }
+    public void ContinueRecording()
+    {
+        _recording = true;
+    }
+}
